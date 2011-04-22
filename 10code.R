f199700a60b45fc8d907a51a6162d99c580eb439
Vaidotas Zemlys <zemlys@gmail.com>
2011-04-22 11:39:13 +0300

    Fixed the bug in the simulation code, now OLS gives the expected results

--- 10code.R
+++ 10code.R
@@ -9,13 +9,14 @@ sim.X <- function(n,m,rho=0.5,sd=1,burn.in=300,...) {
 sim.Y <- function(n,m,infty=10,sde=1,tfun,xfun,...) {
     x <- xfun((infty+1)*n,m,...)
     theta <- tfun(expand.grid(1:m,1:(infty*n)-1),...)
-    X <- x[length(x)-(1:(n*m)-1)]
+    X <- x[sort(length(x)-(1:(n*m)-1))]
     start <- length(x)-n*m
     midas <- foreach(i=1:n,.combine=c) %do% {
         sum(x[start:1+i*m]*theta)
     }
     Y <- 1+midas+rnorm(n,sd=sde)
-    res <- list(X=X,Y=Y,theta=theta)
+    browser()
+    res <- list(X=X,Y=Y,theta=theta,midas=midas,x=x)
     class(res) <- "midas_sim"
     res
 }
