e9aac8342a1ab73eccfddc3d38db7736938f7d9f
Vaidotas Zemlys <zemlys@gmail.com>
2011-04-27 14:39:21 +0300

    Fixed small bug. Added new coefficient function

--- 10code.R
+++ 10code.R
@@ -14,8 +14,8 @@ sim.Y <- function(n,m,infty=10,sde=1,tfun,xfun,...) {
     midas <- foreach(i=1:n,.combine=c) %do% {
         sum(x[start:1+i*m]*theta)
     }
-    Y <- 1+midas+rnorm(n,sd=sde)
-    res <- list(X=X,Y=Y,theta=theta,midas=midas,x=x)
+    Y <- midas+rnorm(n,sd=sde)
+    res <- list(X=X,Y=Y,theta=theta,midas=midas,x=x,n=n,m=m)
     class(res) <- "midas_sim"
     res
 }
@@ -24,6 +24,7 @@ reflow <- function(object,...) UseMethod("reflow")
 
 reflow.midas_sim <- function(object,k) {
     m <- length(object$X)%/%length(object$Y)
+    n <- length(object$Y)
     Y <- object$Y[(k+1):n]
     X <- foreach(i=1:(n-k),.combine=rbind) %do% {
         object$X[(m*(k+1)):1+(i-1)*m]
@@ -35,3 +36,19 @@ reflow.midas_sim <- function(object,k) {
 theta.ar <- function(grid,theta=0.3,...) {    
     theta^(grid[,2])
 }
+
+theta.mid <- function(grid,theta=0.7,...) {
+    m <- max(grid[,1])
+    h <- grid[,2]*m+grid[,1]
+    1000*theta^h
+}
+
+prep.nls <- function(object,k) {
+    mod <- lm(Y~.-1,data=reflow(object,k=k))
+    browser()
+    that <- coef(mod)
+    lst <- list(that=that,grid=expand.grid(1:object$m,0:k))
+    lst
+}
+# mod2 <- nls(that~theta.mid(grid,theta),data=lst,start(theta=0.4))
+ #list(ols=mod,nls=mod2)
