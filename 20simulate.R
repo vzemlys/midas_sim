9faef4598bbb9e7fdd8b8fac7729c24e5c691e87
Vaidotas Zemlys <zemlys@gmail.com>
2011-05-07 17:51:36 +0300

    Added the try with all coefficients positive

--- 20simulate.R
+++ 20simulate.R
@@ -10,8 +10,8 @@ sim5 <- sim.Y.finite(100,3,1000,tfun=theta.us214,xfun=sim.X,lambda=c(-0.01,-0.00
 
 IC5 <- gen.IC(sim5,kmax=15)
 
-sn5u <- fit.lambda(sim5,that~theta.us214(index,lambda,beta),k=12,start=list(lambda=c(-0.01,-0.001),beta=5),trace=TRUE)
+sn5u <- fit.lambda(sim5,that~theta.us214(index,lambda,beta),k=12,start=list(lambda=c(-0.01,-0.001),beta=0.5),trace=TRUE)
 
-sn5r <- fit.lambda(sim5,that~theta.rs214(index,lambda,beta),k=12,start=list(lambda=c(-0.01,-0.001),beta=5),trace=TRUE)
+sn5r <- fit.lambda(sim5,that~theta.rs214(index,lambda,beta),k=12,start=list(lambda=c(-0.01,-0.001),beta=0.5),trace=TRUE)
 
-
+sn5un <- fit.lambda(sim5,that~theta.r214(index,lambda,beta),k=12,start=list(lambda=c(-0.01,-0.001),beta=0.5),trace=TRUE)
